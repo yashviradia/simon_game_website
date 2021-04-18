e92f5d0d727e2244fb864aebfbbb503d0026a33a
yashviradiaofficial <yashviradiaofficial@gmail.com>
2021-04-18 14:41:11 +0200

    update function in website

--- game.js
+++ game.js
@@ -1,7 +1,11 @@
+var userClickedPattern = [];
+
 var gamePattern = [];
 
 
 var buttonColours = ["red", "blue", "green", "yellow"];
+
+playSound(userChosenColour);
 
 function nextSequence() {
   var randomNumber = Math.floor(Math.random() * 4);
@@ -9,6 +13,20 @@ function nextSequence() {
   var randomChosenColour = buttonColours[randomNumber];
 
   gamePattern.push(randomChosenColour);
+
+  $("#" + "randomChosenColour").fadeIn(100).fadeOut(100).fadeIn(100);
+
+  playSound(randomChosenColour);
 }
 
-$("#randomChosenColour")
+
+$(".btn").click(function() {
+  var userChosenColour = $(this).attr("id")
+
+  userClickedPattern.push(userChosenColour)
+});
+
+function playSound(name) {
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
